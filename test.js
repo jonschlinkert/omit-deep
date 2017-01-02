fc5050df2c7aaad98f86992861b0fcbbd444cd34
jonschlinkert <jon.schlinkert@sellside.com>
2017-01-01 20:24:24 -0500

    use assert

--- test.js
+++ test.js
@@ -1,48 +1,49 @@
 'use strict';
 
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var omitDeep = require('./');
 
-describe('.omit()', function () {
-  it('should recursively omit key passed as a string.', function () {
+describe('.omit()', function() {
+  it('should recursively omit key passed as a string.', function() {
     var o = omitDeep({a: 'a', b: 'b', c: {b: 'b', d: 'd', e: {b: 'b', f: 'f', g: {b: 'b', c: 'c'}}}}, 'b')
-    o.should.eql({a: 'a', c: {d: 'd', e: {f: 'f', g: {c: 'c'}}}});
+    assert.deepEqual(o, {a: 'a', c: {d: 'd', e: {f: 'f', g: {c: 'c'}}}});
   });
 
-  it('should recursively omit key passed as an array.', function () {
+  it('should recursively omit key passed as an array.', function() {
     var o = omitDeep({a: 'a', b: 'b', c: {b: 'b', d: 'd', e: {b: 'b', f: 'f', g: {b: 'b', c: 'c'}}}}, ['b'])
-    o.should.eql({a: 'a', c: {d: 'd', e: {f: 'f', g: {c: 'c'}}}});
+    assert.deepEqual(o, {a: 'a', c: {d: 'd', e: {f: 'f', g: {c: 'c'}}}});
   });
 
-  it('should recursively omit multiple keys.', function () {
+  it('should recursively omit multiple keys.', function() {
     var o = omitDeep({a: 'a', b: 'b', c: {b: 'b', d: 'd', e: {b: 'b', f: 'f', g: {b: 'b', c: 'c'}}}}, ['b', 'd', 'f'])
-    o.should.eql({a: 'a', c: {e: {g: {c: 'c'}}}});
+    assert.deepEqual(o, {a: 'a', c: {e: {g: {c: 'c'}}}});
   });
 
-  it('should omit the given keys.', function () {
-    omitDeep({a: 'a', b: 'b', c: 'c'}, ['a', 'c']).should.eql({ b: 'b' });
+  it('should omit the given keys.', function() {
+    assert.deepEqual(omitDeep({a: 'a', b: 'b', c: 'c'}, ['a', 'c']), { b: 'b' });
   });
 
-  it('should return the object if no keys are specified.', function () {
-    omitDeep({a: 'a', b: 'b', c: 'c'}).should.eql({a: 'a', b: 'b', c: 'c'});
+  it('should return the object if no keys are specified.', function() {
+    assert.deepEqual(omitDeep({a: 'a', b: 'b', c: 'c'}), {a: 'a', b: 'b', c: 'c'});
   });
 
-  it('should return an empty object if no object is specified.', function () {
-    omitDeep().should.eql({});
+  it('should return an empty object if no object is specified.', function() {
+    assert.deepEqual(omitDeep(), {});
   });
 
-  it('should return the input unchaged if not an array or an object', function () {
-    omitDeep('foo').should.eql('foo');
+  it('should return the input unchaged if not an array or an object', function() {
+    assert.deepEqual(omitDeep('foo'), 'foo');
   });
 
-  it('should omit keys from objects in arrays', function () {
+  it('should omit keys from objects in arrays', function() {
     var o = omitDeep([
       {a: 'a', b: 'b'},
       [
         {a: 'a', b: 'b'}
       ]
     ], 'b')
-    o.should.eql([
+    assert.deepEqual(o, [
       {a: 'a'},
       [
         {a: 'a'}
@@ -55,7 +56,7 @@ describe('.omit()', function () {
       "numbers": ["1", "2"]
     }, 'nothing')
 
-    o.should.eql({
+    assert.deepEqual(o, {
       "numbers": ["1", "2"]
     });
   })
