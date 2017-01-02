bf19529a04903db43b34107663ccc4749d959f79
jonschlinkert <jon.schlinkert@sellside.com>
2017-01-01 20:39:14 -0500

    return value if `keys` isn't an array or string

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var isObject = require('is-plain-object');
 var forOwn = require('for-own');
 var omit = require('omit-keys');
 
-module.exports = function omitDeep(value, props) {
+module.exports = function omitDeep(value, keys) {
   if (typeof value === 'undefined') {
     return {};
   }
 
   if (Array.isArray(value)) {
     for (var i = 0; i < value.length; i++) {
-      value[i] = omitDeep(value[i], props);
+      value[i] = omitDeep(value[i], keys);
     }
     return value;
   }
@@ -20,10 +20,18 @@ module.exports = function omitDeep(value, props) {
     return value;
   }
 
+  if (typeof keys === 'string') {
+    keys = [keys];
+  }
+
+  if (!Array.isArray(keys)) {
+    return value;
+  }
+
   var o = {};
-  forOwn(value, function(val, key) {
-    o[key] = omitDeep(val, props);
+  forOwn(value, function (val, key) {
+    o[key] = omitDeep(val, keys);
   });
 
-  return omit(o, props);
+  return omit(o, keys);
 };
